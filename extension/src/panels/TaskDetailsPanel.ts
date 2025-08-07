b2879b4e76bf0b9dade65314766838c94b061822
Angel M Miguel <hi@angel.kiwi>
2025-08-07 17:51:09 +0200

    feat: move current views to Lit components and delete old views

--- extension/src/panels/TaskDetailsPanel.ts
+++ extension/src/panels/TaskDetailsPanel.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import { RoverCLI } from '../rover/cli';
-import { TaskDetails } from '../rover/types';
+import { RoverCLI } from '../rover/cli.js';
+import { TaskDetails } from '../rover/types.js';
 
 export class TaskDetailsPanel {
     public static currentPanel: TaskDetailsPanel | undefined;
