0b63b95d08cbea679a5b0e8b0f89bffcb7dbdaf0
Rafael Fernández López <ereslibre@ereslibre.es>
2026-01-22 10:59:29 +0100

    chore: do not re-export generateRandomId

--- packages/cli/src/utils/branch-name.ts
+++ packages/cli/src/utils/branch-name.ts
@@ -1,8 +1,5 @@
 import { generateRandomId } from 'rover-core';
 
-// Re-export generateRandomId for backward compatibility
-export { generateRandomId };
-
 /**
  * Generate a unique branch name for a task
  * Format: rover/task-{TASK_ID}-{NANOID_RANDOM_STRING}
